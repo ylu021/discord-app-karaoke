102e25173a3ee146871c869bc49e2df70d1e5a31
ylu021 <yichen.luu@gmail.com>
2025-02-26 09:20:42 -0500

    add form

--- src/app/Activity.tsx
+++ src/app/Activity.tsx
@@ -1,21 +1,30 @@
+import { useState } from 'react'
 import { Player } from './Player'
+import LandingForm, { FormInput } from './LandingForm'
+import Lyrics from './Lyrics'
 
 export const URL = 'https://www.youtube.com/watch?v=jWQx2f-CErU'
 
 export const Activity = () => {
+	const [showPlayer, setShowPlayer] = useState(false)
+	const [songInfo, setSongInfo] = useState({
+		title: 'Whipsplash',
+		artist: 'Aespa'
+	})
+
+	const updateSongInfo = (songInfo: FormInput) => {
+		setSongInfo(songInfo)
+	}
+
 	return (
 		<div>
 			<img src="/rocket.png" className="logo" alt="Discord" />
 			<h1>Karaoke Room</h1>
-			<Player url={URL} />
-			<p>
-				<small>
-					Powered by{' '}
-					<a className="robojs" href="https://roboplay.dev/docs">
-						Robo.js
-					</a>
-				</small>
-			</p>
+			{showPlayer ? (
+				<Player url={URL} songInfo={songInfo} />
+			) : (
+				<LandingForm setShowPlayer={setShowPlayer} updateSongInfo={updateSongInfo} />
+			)}
 		</div>
 	)
 }
---
+++ src/app/LandingForm.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+
+export interface FormInput {
+	url?: string
+	title: string
+	artist: string
+}
+
+export default function LandingForm({
+	setShowPlayer,
+	updateSongInfo
+}: {
+	setShowPlayer: React.Dispatch<React.SetStateAction<boolean>>
+	updateSongInfo: Function
+}) {
+	const [formInput, setFormInput] = useState({
+		url: '',
+		title: '',
+		artist: ''
+	} as FormInput)
+
+	const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		if (formIsEmpty()) return
+		setShowPlayer(true)
+		updateSongInfo(formInput)
+	}
+
+	const handleUpdateVal = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const updatedFormInput = { ...formInput }
+		const name = e.target.name as keyof FormInput
+		updatedFormInput[name] = e.target.value
+		setFormInput(updatedFormInput)
+	}
+
+	const formIsEmpty = () => {
+		return !formInput.url.trim() || !formInput.title.trim() || !formInput.artist.trim()
+	}
+	return (
+		<div>
+			<form onSubmit={handleSearch} className="flex flex-col gap-4 mt-8">
+				<input
+					type="text"
+					name="url"
+					value={formInput.url}
+					onChange={handleUpdateVal}
+					placeholder="Paste a youtube karaoke video here"
+				/>
+				<input
+					type="text"
+					name="title"
+					value={formInput.title}
+					onChange={handleUpdateVal}
+					placeholder="Enter the song title"
+				/>
+				<input
+					type="text"
+					name="artist"
+					value={formInput.artist}
+					onChange={handleUpdateVal}
+					placeholder="Enter the artist name"
+				/>
+				<button
+					type="submit"
+					disabled={formIsEmpty()}
+					className={
+						formIsEmpty()
+							? 'disabled:pointer-events-none disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 disabled:shadow-none'
+							: ''
+					}
+				>
+					Submit
+				</button>
+			</form>
+		</div>
+	)
+}
